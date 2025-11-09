542bed162a2634e4cd76f0c8a002b3b3c156204f
ceron1997 <noeceron43@gmail.com>
2025-11-08 22:24:00 -0600

    Estructura base inicial del proyecto Flutter Pomodoro

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.green,
+  Colors.purple,
+  Colors.pink,
+  Color.fromARGB(255, 74, 255, 64)
+];
+
+class AppTheme {
+  final int selectedColor;
+  final bool isDarkmode;
+
+  AppTheme({this.selectedColor = 0, this.isDarkmode = true})
+      : assert(selectedColor >= 0, "El selectedColor debe ser mayor a cero"),
+        assert(selectedColor < colorList.length,
+            "El selectedColor debe ser menor a ${colorList.length - 1}");
+
+  ThemeData getTheme() => ThemeData(
+        brightness: isDarkmode ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+
+
+
+  AppTheme copyWith({int? selectedColor, bool? isDarkmode}) => AppTheme(
+        selectedColor: selectedColor ?? this.selectedColor,
+        isDarkmode: isDarkmode ?? this.isDarkmode,
+      );
+}
